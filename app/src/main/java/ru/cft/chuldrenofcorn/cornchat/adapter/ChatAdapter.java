a768bc37c6a1177a05722b7a26e961e1b0281147
Alexander Zhukov <al.zhukov@ftc.ru>
2016-08-26 16:56:20 +0700

    Added some UI

--- app/src/main/java/ru/cft/chuldrenofcorn/cornchat/adapter/ChatAdapter.java
+++ app/src/main/java/ru/cft/chuldrenofcorn/cornchat/adapter/ChatAdapter.java
@@ -1,50 +1,106 @@
 package ru.cft.chuldrenofcorn.cornchat.adapter;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.SimpleAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import ru.cft.chuldrenofcorn.cornchat.R;
 import ru.cft.chuldrenofcorn.cornchat.dto.ChatMessage;
 
 /**
  * Created by azhukov on 26/08/16.
  */
-public class ChatAdapter extends BaseAdapter {
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
 
-    private ArrayList<ChatMessage> messages;
+    private Context context;
+    private ArrayList<ChatMessage> messages = new ArrayList<>(10);
 
-    public ChatAdapter() {
-    }
-
-    public ChatAdapter(final ArrayList<ChatMessage> messages) {
-        this.messages = messages;
+    public ChatAdapter(final Context context) {
+        this.context = context;
     }
 
     @Override
-    public int getCount() {
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_chat, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, final int position) {
+        viewHolder.bind(messages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
         return messages.size();
     }
 
-    @Override
-    public Object getItem(final int i) {
-        return messages.get(i);
-    }
-
-    @Override
-    public long getItemId(final int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(final int i, final View view, final ViewGroup viewGroup) {
-        return null;
-    }
-
-    public void add(final ChatMessage chatMessage) {
-        messages.add(chatMessage);
+    public void setMessages(final ArrayList<ChatMessage> messages) {
+        this.messages = messages;
         notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private LinearLayout containerLinearLayout;
+        private LinearLayout bubbleLinearLayout;
+
+        private TextView textViewName;
+        private TextView textViewMessage;
+        private TextView textViewDate;
+
+        public void bind(final ChatMessage chatMessage) {
+            textViewName.setText(chatMessage.getSenderName());
+            textViewMessage.setText(chatMessage.getText());
+            textViewDate.setText(chatMessage.getDate());
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            if (chatMessage.isLocal()) {
+                bubbleLinearLayout.setBackgroundResource(R.drawable.ic_outcoming);
+
+                textViewName.setText(chatMessage.getSenderName());
+
+                textViewName.setTextColor(context.getResources().getColor(R.color.purple100));
+                textViewMessage.setTextColor(Color.BLACK);
+                textViewDate.setTextColor(context.getResources().getColor(R.color.purple400));
+
+                layoutParams.gravity = Gravity.END;
+                textViewDate.setLayoutParams(layoutParams);
+
+                containerLinearLayout.setGravity(Gravity.END);
+            } else {
+                bubbleLinearLayout.setBackgroundResource(R.drawable.ic_incoming);
+
+                textViewName.setText(chatMessage.getSenderName());
+
+                textViewName.setTextColor(context.getResources().getColor(R.color.purple100));
+                textViewMessage.setTextColor(Color.WHITE);
+                textViewDate.setTextColor(context.getResources().getColor(R.color.purple100));
+
+                layoutParams.gravity = Gravity.START;
+                textViewDate.setLayoutParams(layoutParams);
+
+                containerLinearLayout.setGravity(Gravity.START);
+            }
+        }
+
+        ViewHolder(final View itemView) {
+            super(itemView);
+            textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            textViewMessage = (TextView) itemView.findViewById(R.id.textViewMessage);
+            textViewDate = (TextView) itemView.findViewById(R.id.textViewDate);
+
+            containerLinearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayoutMessageContainer);
+            bubbleLinearLayout = (LinearLayout) itemView.findViewById(R.id.linearLayoutBubble);
+        }
     }
 }
--- app/src/main/java/ru/cft/chuldrenofcorn/cornchat/dto/ChatMessage.java
+++ app/src/main/java/ru/cft/chuldrenofcorn/cornchat/dto/ChatMessage.java
@@ -8,11 +8,13 @@ public class ChatMessage {
     private String senderName;
     private String text;
     private String date;
+    private boolean isLocal; //если true - сообщение написал пользователь, если false - пришло из вне
 
-    public ChatMessage(final String senderName, final String text, final String date) {
+    public ChatMessage(final String senderName, final String text, final String date, final boolean isLocal) {
         this.senderName = senderName;
         this.text = text;
         this.date = date;
+        this.isLocal = isLocal;
     }
 
     public String getSenderName() {
@@ -30,5 +32,9 @@ public class ChatMessage {
     @Override
     public String toString() {
         return text;
+    }
+
+    public boolean isLocal() {
+        return isLocal;
     }
 }
